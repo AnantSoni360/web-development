599fecb318c8d6ff43c6c26e0e69485aec3bfd85
AnantSoni360 <147417339+AnantSoni360@users.noreply.github.com>
2023-12-23 17:13:17 +0530

    files added

---
+++ kisan vikas/anant.py
@@ -0,0 +1,3 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide the user with an option to look it up.
+a={"mother":"ਮਾਂ","father":"पिता","friends":"दोस्तों","brother":"भाई"}
+print (a)
